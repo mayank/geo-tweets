61356a0dfc65a03b7820637904f16aebefbd9e34
mayank <nitb.scholar@gmail.com>
2017-01-08 22:55:44 +0530

    updated API, not required oauth token

--- api/twitter.js
+++ api/twitter.js
@@ -2,27 +2,24 @@ var twitterAPI = require('node-twitter-api');
 var chalk = require('chalk');
 var config = require('./twitter.config')
 
-const debug = false;
-
 var twitter = twitterAPI(config.keys);
 
-exports.getTweetsByLocation = function(geo, callback){
+exports.getTweetsByLocation = function(geo, callback, query){
 	var status = [];
 	twitter.search({ 
-		q: '',
+		q: query || '',
 		geocode: geo.lat+','+geo.lng+',1mi',
 		type: 'recent',
 		lang: 'en',
 		count: 100
 	},
-	config.tokens.accessToken, 
-	config.tokens.secretToken,
+	null, 
+	null,
 	function(error, data){
 		if(error){ console.log(error); return; }
 		
 		for(var i=0;i<data.statuses.length;i++){
 			status.push(data.statuses[i]);
-			if(debug) onsole.log(chalk.green(status.user.name)+'['+chalk.red('@'+status.user.screen_name)+'] '+status.text);			
 		}
 		callback(status)
 	});
